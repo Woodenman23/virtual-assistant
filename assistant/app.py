59866d4e17c746d382d59fc4d50e5a147a2dc836
Woodenman23 <josephrfoster@protonmail.com>
2024-06-17 22:11:45 +0100

    Tell the Time in San Francisco

--- assistant/app.py
+++ assistant/app.py
@@ -1,14 +1,19 @@
+from datetime import datetime
+import pytz
 import typer
 
 app = typer.Typer()
 
 @app.command()
-def hello(name: str):
-    print(f"Hello {name}")
+def tell_time(city="San Francisco"):
+    city_tz = pytz.timezone("US/Pacific")
+    city_datetime = datetime.now(city_tz)
+    city_current_time = city_datetime.strftime("%H:%M:%S")
+    print(f"The time in {city} is {city_current_time}")
 
 @app.command()
-def goodbye():
-    print("Goodbye")
+def zones():
+    print(pytz.all_timezones)
 
 def main():
     app()
