cbe897543f45a963161dab53a6400555b9a305a5
Woodenman23 <josephrfoster@protonmail.com>
2024-06-19 20:20:26 +0100

    Generalised Tell Time Command for Major Cities

--- assistant/app.py
+++ assistant/app.py
@@ -1,15 +1,23 @@
 from datetime import datetime
+import json
 import pytz
 import typer
 
 app = typer.Typer()
 
+# cities with 2 or more words require apostraphes
 @app.command()
-def tell_time(city="San Francisco"):
-    city_tz = pytz.timezone("US/Pacific")
-    city_datetime = datetime.now(city_tz)
+def tell_time(city: str):
+    time_zone = get_time_zone(city)
+    pytz_time_zone = pytz.timezone(time_zone)
+    city_datetime = datetime.now(pytz_time_zone)
     city_current_time = city_datetime.strftime("%H:%M:%S")
-    print(f"The time in {city} is {city_current_time}")
+    print(f"The time in {city.capitalize()} is {city_current_time}")
+
+def get_time_zone(city: str):
+    with open("assistant/data/cities.json", "r") as file:
+        time_zones = json.load(file)
+        return time_zones[city.lower()]
 
 @app.command()
 def zones():
